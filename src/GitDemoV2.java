82a254a91de4a05f223075d50310eecb663d042e
Lenovo <toarfath@gmail.com>
2023-07-25 18:59:44 +0530

    This is my 2nd Commit

---
+++ src/GitDemoV2.java
@@ -0,0 +1,26 @@
+
+public class GitDemoV2 {
+
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+
+			String hungry = "Yes";
+
+			
+				if (hungry == "yes") {
+
+					System.out.println("Eat something ");
+
+				} 
+				
+				else {
+					System.out.println("Do work");
+
+				}
+			}
+
+
+		}
+
+}
